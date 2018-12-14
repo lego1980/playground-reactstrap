de69f9bff1ad2c9a27e34f0eaa6e86762b1d764f
Ronald Ayson <ronaldayson@mackbookpro.local>
2018-12-14 10:56:54 -0800

    Added bootstrap reactrap with danger button example

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const DangerButton = (props) => {
+    return (
+        <Button color="danger">Danger!</Button>
+    )        
+}
